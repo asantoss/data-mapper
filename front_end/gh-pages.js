976b515f4da7155fa2e2c551e33e6e08fe51e22f
Alexander Santos <alexsantosantana@live.com>
2022-12-20 14:02:03 -0500

    Update the build

---
+++ front_end/gh-pages.js
@@ -0,0 +1,16 @@
+var ghpages = require('gh-pages');
+
+ghpages.publish(
+	'public', // path to public directory
+	{
+		branch: 'gh-pages',
+		repo: 'https://github.com/asantoss/data-mapper.git', // Update to point to your repository
+		user: {
+			name: 'Alexander Santos', // update to use your name
+			email: 'alexsantosantana@live.com' // Update to use your email
+		}
+	},
+	() => {
+		console.log('Deploy Complete!');
+	}
+);
--- front_end/svelte.config.js
+++ front_end/svelte.config.js
@@ -1,4 +1,5 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
+
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,7 +9,11 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			pages: 'build', // path to public directory
+			assets: 'build', // path to public directory
+			fallback: null
+		})
 	}
 };
 
